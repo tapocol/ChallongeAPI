57547c142d6884ec9afd3ac707646074f3e78b3f
mano82 <mano82.ita@gmail.com>
2015-02-27 22:00:55 +0100

    Update Tournament.php
    modified paths and added methods for updated api v1

--- src/Challonge/Tournament.php
+++ src/Challonge/Tournament.php
@@ -5,44 +5,61 @@
  */
 class ChallongeTournament extends ChallongeAPI
 {
-    public function reqIndex()
+	const PREFIX = "tournaments";
+	
+	public function reqIndex()
     {
-        return $this->request();
+        return $this->request(self::PREFIX);
     }
 
     public function reqCreate()
     {
-        return $this->request('', 'post');
+        return $this->request(self::PREFIX, 'post');
     }
 
     public function reqShow($id)
     {
-        return $this->request("/$id");
+        return $this->request(self::PREFIX."/$id");
     }
 
     public function reqUpdate($id)
     {
-        return $this->request("/$id", 'put');
+        return $this->request(self::PREFIX."/$id", 'put');
     }
 
     public function reqDestroy($id)
     {
-        return $this->request("/$id", 'delete');
+        return $this->request(self::PREFIX."/$id", 'delete');
     }
 
-    public function reqPublish($id)
+    /*public function reqPublish($id)
     {
         return $this->request("/publish/$id", 'post');
-    }
+    }*/
 
     public function reqStart($id)
     {
-        return $this->request("/start/$id", 'post');
+        return $this->request(self::PREFIX."/$id/start", 'post');
     }
 
     public function reqReset($id)
     {
-        return $this->request("/reset/$id", 'post');
+        return $this->request(self::PREFIX."/$id/reset", 'post');
+    }
+	
+	public function reqFinalize($id)
+    {
+        return $this->request(self::PREFIX."/$id/finalize", 'post');
+    }
+	
+	public function reqProcessCheckIns($id)
+    {
+        return $this->request(self::PREFIX."/$id/process_check_ins", 'post');
+    }
+	
+	public function reqAbortCheckIn($id)
+    {
+        return $this->request(self::PREFIX."/$id/abort_check_in", 'post');
     }
 }
 
